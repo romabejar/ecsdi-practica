9a5ed8bf8a7ffd2aeeaae95c9638e01c6cbb20b1
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-05 22:36:47 +0200

    modificaciones AgExt

--- Practica/AgExtActividades.py
+++ Practica/AgExtActividades.py
@@ -9,6 +9,7 @@ import requests
 from requests import ConnectionError
 from multiprocessing import Process
 from googleplaces import GooglePlaces, types, lang
+import string
 
 # Definimos los parametros de la linea de comandos
 parser = argparse.ArgumentParser()
@@ -20,9 +21,14 @@ parser.add_argument('--messages', nargs='+', default=[], help="mensajes a enviar
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def isalive():
+def isAlive():
+    text = 'Hi i\'m AgExtActividades o/, if you wanna party go to <a href= /comm>here</a>'
+    return text
+
+
+@app.route("/comm")
+def getPlaces():
     """
     Entrada del servicio para saber si esta en funcionamiento
     :return:
@@ -33,26 +39,29 @@ def isalive():
 
     # You may prefer to use the text_search API, instead.
     query_result = google_places.nearby_search(
-        location='London, England', keyword='Fish and Chips',
-        radius=20000, types=[types.TYPE_FOOD])
-
+        location='Barcelona, Spain', keyword='Discoteca',
+        radius=20000, types=[types.TYPE_NIGHT_CLUB])
     # placestring = "Name: %s, GeoLoc: %s, Reference: %s, Phone: %s \n"(places[0].name, places[0].geo_location, places[0].reference, places[0].local_phone_number)
-    return query_result.places[0].name
-    # for place in query_result.places:
+    resultado = ""
+    for place in query_result.places:
         # Returned places from a query are place summaries.
-        # return place.name
-        # return place.geo_location
-        # return place.reference
+        print place.name
+        print place.geo_location
+        print place.reference
+        resultado += str(place.name) + str("\r\n\r\n")
+
 
         # The following method has to make a further API call.
         # place.get_details()
         # Referencing any of the attributes below, prior to making a call to
         # get_details() will raise a googleplaces.GooglePlacesAttributeError.
-        # return place.details # A dict matching the JSON response from Google.
-        # return place.local_phone_number
-        # return place.international_phone_number
-        # return place.website
-        # return place.url
+        # print place.details # A dict matching the JSON response from Google.
+        # print place.local_phone_number
+        # print place.international_phone_number
+        # print place.website
+        # print place.url
+
+    return resultado
 
 if __name__ == '__main__':
 
