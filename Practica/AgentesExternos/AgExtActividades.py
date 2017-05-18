c93c86815cf9aaad84a5da27cfbdaff1a89c2049
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-18 15:58:01 +0200

    agente act externo working :)

--- Practica/AgentesExternos/AgExtActividades.py
+++ Practica/AgentesExternos/AgExtActividades.py
@@ -6,6 +6,7 @@ __author__ = 'bejar'
 from flask import Flask, request, Response
 from flask.json import jsonify
 import json
+import decimal
 import argparse
 import requests
 from requests import ConnectionError
@@ -42,25 +43,27 @@ def getPlaces():
     keyword = request.args["keyword"]
     type = request.args["type"]
 
-    print location
-    print keyword
-    print type
-
     # You may prefer to use the text_search API, instead.
     query_result = google_places.nearby_search(
         location=location, keyword=keyword,
         radius=20000, types=type)
     # placestring = "Name: %s, GeoLoc: %s, Reference: %s, Phone: %s \n"(places[0].name, places[0].geo_location, places[0].reference, places[0].local_phone_number)
-    resultado = ""
-    place_json = ""
+
+    resultado = {}
+    i = 0
     for place in query_result.places:
+        place_json = {}
         # Returned places from a query are place summaries.
-        print place.name
+        # print place.name
         # print place.geo_location
         # print place.reference
-
+        place.get_details()
+        place_json['name'] = place.name
+        place_json['lat'] = float(place.geo_location['lat'])
+        place_json['lng'] = float(place.geo_location['lng'])
+        # place_json['reference'] = place.reference
+        # place_json['details'] = place.details
         # The following method has to make a further API call.
-        # place.get_details()
         # Referencing any of the attributes below, prior to making a call to
         # get_details() will raise a googleplaces.GooglePlacesAttributeError.
         # print place.details # A dict matching the JSON response from Google.
@@ -68,25 +71,24 @@ def getPlaces():
         # print place.international_phone_number
         # print place.website
         # print place.url
+        resultado[i] = place_json
+        print i
+        print resultado[i]
+        i = i+1
 
     # HAY QUE INTENTAR DEVOLVER UN JSON CON LA INFO
-    return 1
+
+    json_data = json.dumps(resultado)
+    print json_data
+    return json_data
+    #return resultado
 
 if __name__ == '__main__':
 
     # parsing de los parametros de la linea de comandos
     args = parser.parse_args()
 
-    # Ponemos en marcha el comportamiento si se indica una direccion
-    if args.acomm is not None:
-        ab = Process(target=behavior, args=(args.messages, (args.acomm, args.aport)))
-        ab.start()
-
     # Ponemos en marcha el servidor
     app.run(host=args.host, port=args.port)
-
-    # Esperamos a que acaben los behaviors
-    if args.acomm is not None:
-        ab.join()
 
     print 'The End'
