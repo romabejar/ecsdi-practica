5acc536dd5c6717ee18b7826d0298a4908913fb0
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-24 20:21:44 +0200

    some random stuff

--- Practica/AgentesExternos/AgExtActividades.py
+++ Practica/AgentesExternos/AgExtActividades.py
@@ -35,51 +35,6 @@ def getPlaces():
     /place?location=loc&keyword=key&type=type
     :return:
     """
-    YOUR_API_KEY = 'AIzaSyCyjudYWWbnReJa3LdTgfnQXgLxIyXvLSk'
-    google_places = GooglePlaces(YOUR_API_KEY)
-
-    location = request.args["location"]
-    keyword = request.args["keyword"]
-    type = request.args["type"]
-
-    # You may prefer to use the text_search API, instead.
-    query_result = google_places.nearby_search(
-        location=location, keyword=keyword,
-        radius=20000, types=type)
-    # placestring = "Name: %s, GeoLoc: %s, Reference: %s, Phone: %s \n"(places[0].name, places[0].geo_location, places[0].reference, places[0].local_phone_number)
-
-    resultado = {}
-    i = 0
-    for place in query_result.places:
-        place_json = {}
-        # Returned places from a query are place summaries.
-        # print place.name
-        # print place.geo_location
-        # print place.reference
-        place.get_details()
-        place_json['name'] = place.name
-        place_json['lat'] = float(place.geo_location['lat'])
-        place_json['lng'] = float(place.geo_location['lng'])
-        # place_json['reference'] = place.reference
-        # place_json['details'] = place.details
-        # The following method has to make a further API call.
-        # Referencing any of the attributes below, prior to making a call to
-        # get_details() will raise a googleplaces.GooglePlacesAttributeError.
-        # print place.details # A dict matching the JSON response from Google.
-        # print place.local_phone_number
-        # print place.international_phone_number
-        # print place.website
-        # print place.url
-        resultado[i] = place_json
-        print i
-        print resultado[i]
-        i = i+1
-
-    # HAY QUE INTENTAR DEVOLVER UN JSON CON LA INFO
-
-    json_data = json.dumps(resultado)
-    print json_data
-    return json_data
 
 if __name__ == '__main__':
 
